237b014300f4dbaf0b0c1d10ad5a6397ecb9101b
Franck Mamalet <49721198+franckma31@users.noreply.github.com>
2025-02-19 10:54:33 +0100

    update dataset_tools

--- src/dataset_tools/__init__.py
+++ src/dataset_tools/__init__.py
@@ -1,7 +1,5 @@
-import dataset_tools.add_task
 import dataset_tools.permuted_mnist
 import dataset_tools.copy_task
-import dataset_tools.penn_tree_bank
 import dataset_tools.sequential_mnist
 import dataset_tools.imdb
 import dataset_tools.glue
